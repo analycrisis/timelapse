532389d1ce09baeeb40dbc21d697757a9ab18d85
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2021-10-27 19:42:53 +0700

    Update projects

--- projects/mukpakpak.js
+++ projects/mukpakpak.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://nunmun.vercel.app')
+    await page.goto('https://mukpakpak.tinnaphat.xyz/')
     await capture(page, 'home')
   },
 })
