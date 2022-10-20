99344c02fdc4e29f7c764a98f2e010a7ac560398
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2022-10-20 20:53:29 +0700

    Update mukpakpak.config.js

--- projects/mukpakpak.config.js
+++ projects/mukpakpak.config.js
@@ -2,7 +2,7 @@ module.exports = /** @type {TimelapseProjectConfig} */ ({
   async run({ page, capture }) {
     await page.setViewport({ width: 1280, height: 720 })
 
-    await page.goto('https://mukpakpak.tinnaphat.xyz/')
+    await page.goto('https://thjokes.tin-sci.me/')
     await capture(page, 'home')
   },
 })
